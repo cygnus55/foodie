e617aa9c585d711c957600d6e8f42977bf5f2c84
Bishal Joshi <bishaljoshi08@gmail.com>
2022-02-04 16:29:19 +0545

    initial: First commit

---
+++ frontend/foodie/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Foodie APP',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Foodie'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text(
+          'This is Home Page',
+        ),
+      ),
+    );
+  }
+}
---
+++ logsheet.md
@@ -0,0 +1,4 @@
+# Weekly Logsheet
+
+## Week 1:
+
