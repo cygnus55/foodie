e80103660649c152ead45f28ae761dbec7a4818e
deependra12 <dg02432018@student.ku.edu.np>
2022-02-10 14:07:36 +0545

    basic login page

---
+++ frontend/foodie/lib/screens/login_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({ Key? key }) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(children: [
+        Container(
+          width: double.infinity,
+          height:MediaQuery.of(context).size.height*0.33,
+          child: Image.asset('assets/images/login.png',fit:BoxFit.cover),
+        ),
+        const SizedBox(height: 25,),
+        const Text("Bringing Restaurant to you",style:TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+      ],)
+      
+    );
+  }
+}
