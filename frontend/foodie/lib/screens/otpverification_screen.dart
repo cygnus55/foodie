e5db38a001779e007ba40e0de7cf77272cdac298
deependra12 <dg02432018@student.ku.edu.np>
2022-02-16 14:13:50 +0545

    Add Button disabled functionality

--- frontend/foodie/lib/screens/otpverification_screen.dart
+++ frontend/foodie/lib/screens/otpverification_screen.dart
@@ -8,41 +8,75 @@ class OtpVerificationScreen extends StatefulWidget {
 }
 
 class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
+  bool isEnabled = false;
+  final otpController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: const Text('Otp Vertifcation'),centerTitle: true,),
+        appBar: AppBar(
+          title: const Text('Otp Vertifcation'),
+          centerTitle: true,
+        ),
         body: Center(
-          child: Column(
-            children: [
-               const SizedBox(height: 40,),
-            Text('We have sent a verification code to ',
-             style: Theme.of(context).textTheme.subtitle1,),
-             Text('+977 9808074162' ,
-             style: Theme.of(context).textTheme.headline5?.copyWith(fontWeight: FontWeight.bold,fontSize: 15)
-             ,),
-               const SizedBox(height: 10,),
-               Container(
-                 margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.2),
-                 child: const TextField(
-                   decoration: InputDecoration(
-                     hintText: 'Enter Otp',
-                     border: OutlineInputBorder()
-                   ),
-                   autofocus: true,
-                   maxLength: 6,
-                   keyboardType: TextInputType.number,
-                 )),
-                 Row(mainAxisAlignment: MainAxisAlignment.center,
-                   children:  [
-                   const Text("Didn't receive a code ?"),
-                   TextButton(onPressed: (){}, child:const Text('Resend'))
-                 ],),
-                 SizedBox(
-                   width: MediaQuery.of(context).size.width*0.7,
-                   child: const ElevatedButton(onPressed: null, child: Text('Proceed')))
-    
+          child: Column(children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              'We have sent a verification code to ',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text(
+              '+977 9808074162',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.copyWith(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.2),
+                child: TextField(
+                  decoration: const InputDecoration(
+                      hintText: 'Enter Otp', border: OutlineInputBorder()),
+                  autofocus: true,
+                  maxLength: 6,
+                  keyboardType: TextInputType.number,
+                  controller: otpController,
+                  onChanged: (string) => {
+                    if (string.length == 6)
+                      {
+                        setState(
+                          () {
+                            isEnabled = true;
+                          },
+                        )
+                      }
+                    else
+                      {
+                        setState(() {
+                          isEnabled = false;
+                        })
+                      }
+                  },
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Didn't receive a code ?"),
+                TextButton(onPressed: () {}, child: const Text('Resend'))
+              ],
+            ),
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: ElevatedButton(
+                    onPressed: isEnabled ? () {} : null,
+                    child: const Text('Proceed')))
           ]),
         ),
       ),
