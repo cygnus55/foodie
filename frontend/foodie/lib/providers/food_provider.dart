ea918fc60cd4cfea7cdeea80511d4f619e3e33c2
Bishal Joshi <bishaljoshi08@gmail.com>
2022-02-21 16:02:32 +0545

    food provider

--- frontend/foodie/lib/providers/food_provider.dart
+++ frontend/foodie/lib/providers/food_provider.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Food with ChangeNotifier {
+  String? id;
+  String? name;
+  String? description;
+  double? price;
+  String? image;
+  bool? isAvailable = true;
+  int? discountPercent;
+  // Map<String,dynamic>? restaurant;
+  // List? tags;
+  bool? isVeg;
+
+  Food({
+    this.id,
+    this.name,
+    this.description,
+    this.price,
+    this.image,
+    this.discountPercent,
+    this.isVeg,
+  });
+
   void addFood() {
     notifyListeners();
   }
