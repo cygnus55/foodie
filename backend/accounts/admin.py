ba8b835c22f2374130d4305f4659972b93ceb3ce
Shreyam Pokharel <pshreyam@gmail.com>
2022-02-21 17:01:48 +0545

    Configure admin views for defined models

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -5,6 +5,6 @@ from accounts.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['mobile', 'username', 'email', 'is_customer', 'is_restaurant', 'is_delivery_person']
-    search_fields = ['mobile', 'email', 'username']
-    list_filter = ['is_customer', 'is_restaurant', 'is_delivery_person']
+    list_display = ["full_name", "mobile", "username", "email", "is_customer", "is_restaurant", "is_delivery_person"]
+    search_fields = ["full_name", "mobile", "email", "username"]
+    list_filter = ["is_customer", "is_restaurant", "is_delivery_person"]
