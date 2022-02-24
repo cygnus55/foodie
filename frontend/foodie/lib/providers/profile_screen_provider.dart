2c95b77c2a8b5116df19ccc0199eba82553e9ac3
deependra12 <dg02432018@student.ku.edu.np>
2022-02-24 20:03:39 +0545

    Fetch user data

--- frontend/foodie/lib/providers/profile_screen_provider.dart
+++ frontend/foodie/lib/providers/profile_screen_provider.dart
@@ -5,27 +5,35 @@ import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
-import '../models/httpexception.dart';
-
 class Profile with ChangeNotifier {
-  String? _userId;
-  String? _userName;
-  String? _userEmail;
+  Map userProfile = {
+    'username': '',
+    'userid': '',
+    'userMobile': '',
+    'userEmail': '',
+    'userProfilePicture': '',
+  };
+  // String? _userId;
+  // String? _userName;
+  // String? _userEmail;
   final String authToken;
-  String? _userPhone;
+  // String? _userPhone;
 
-  String? get userName {
-    return _userName;
+  // String? get userName {
+  //   return _userName;
+  // }
+
+  Map? get userprofile {
+    return {...userProfile};
   }
 
   Profile(this.authToken);
 
   Future<void> getAccountDetails() async {
     try {
-      var url = Uri.http('10.0.2.2:8000', 'accounts/details/');
+      var url = Uri.http('10.0.2.2:8000', 'customers/profile');
 
       final http.Response response = await http.get(
         url,
@@ -35,13 +43,47 @@ class Profile with ChangeNotifier {
       );
 
       final responseData = json.decode(response.body);
-      print(responseData);
-      _userName = responseData['full_name'];
-      userName;
+      // print(responseData);
+      // _userName = responseData['full_name'];
+      // userName;
+      userProfile['username'] = responseData['full_name'];
+      userProfile['userid'] = responseData['id'];
+      userProfile['userMobile'] = responseData['mobile'];
+      userProfile['userEmail'] = responseData['email'];
+      userProfile['userProfilePicture'] = responseData['profile_picture'];
+
+      userprofile;
+      // print(userprofile);
+
       notifyListeners();
-      print(_userName);
+      // print(_userName);
     } catch (error) {
-      print(error);
+      rethrow;
+    }
+  }
+
+  Future<void> changeProfilePicture(String image) async {
+    try {
+      var url = Uri.http('10.0.2.2:8000', 'customers/profile/');
+
+      // ignore: unused_local_variable
+
+      final http.Response response = await http.patch(
+        url,
+        headers: {
+          'Authorization': 'Token ' + authToken,
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'profile_picture': image,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      print(responseData);
+    } catch (error) {
+      rethrow;
     }
   }
 }
