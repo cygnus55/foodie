09991dd4b4c563b27b54fc69243b02489dee2145
Shreyam Pokharel <pshreyam@gmail.com>
2022-02-28 15:50:54 +0545

    Add reviews feature for foods and restaurants

--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -9,6 +9,9 @@ class RestaurantSerializer(TaggitSerializer, serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="restaurants:restaurant_details")
     user = UserSerializer(read_only=True)
     tags = TagListSerializerField()
+    average_ratings = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    ratings_count = serializers.IntegerField()
+
     class Meta:
         model = Restaurant
         exclude = ('created', 'updated')
---
+++ backend/reviews/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+from customers.models import Customer
+
+
+class Review(models.Model):
+    customer = models.OneToOneField(
+        Customer,
+        related_name="review",
+        on_delete=models.CASCADE
+    )
+    ratings = models.PositiveIntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
+    comment = models.TextField(blank=True)
+    content_type = models.ForeignKey(ContentType, related_name="type", on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    sentiment_score = models.DecimalField(default=0, max_digits=10, decimal_places=7)
+    created = models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review: {self.customer.user.full_name} for {self.content_object}"
