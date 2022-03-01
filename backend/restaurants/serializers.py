d3ce00246fbdcc4dc764d921e34fca099696a032
Shreyam Pokharel <pshreyam@gmail.com>
2022-03-01 16:53:38 +0545

    Add location for restaurants

--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -11,6 +11,8 @@ class RestaurantSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
     average_ratings = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
     ratings_count = serializers.IntegerField()
+    open_hour = serializers.DateTimeField(format="%I:%M %p")
+    close_hour = serializers.DateTimeField(format="%I:%M %p")
 
     class Meta:
         model = Restaurant
