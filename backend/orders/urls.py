594fb0e2b9daaa76a9d6097edc17545640f9c471
RamrajCh <rc02431618@student.ku.edu.np>
2022-03-23 21:37:29 +0545

    add api for delivery location and geocoding

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,11 +1,13 @@
 from ast import Or
 from django.urls import path
 
-from orders.views import OrderCreate, OrderList
+from orders.views import OrderCreate, OrderList, DeliveryLocation, Geocoding
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', OrderCreate.as_view(), name='create'),
     path('', OrderList.as_view(), name='list'),
+    path('delivery-location/', DeliveryLocation.as_view(), name='delivery_location'),
+    path('geocode/', Geocoding.as_view(), name='geocoding'),
 ]
