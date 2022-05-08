48ca77c84a580a099e126a1258f1f3458da93498
RamrajCh <rc02431618@student.ku.edu.np>
2022-05-08 08:37:00 +0545

    Add search api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from api.views import APIRoot
+from api.views import APIRoot, Search
 
 
 app_name = "api"
 
 urlpatterns = [
     path("", APIRoot.as_view(), name="api-root"),
+    path("search/", Search.as_view(), name="search")
 ]
