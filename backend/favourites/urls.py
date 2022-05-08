7e6bd409f99d6dd069a3c75f2e66b6b14f304f7b
RamrajCh <rc02431618@student.ku.edu.np>
2022-05-08 19:07:47 +0545

    Favourite api

--- backend/favourites/urls.py
+++ backend/favourites/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from favourites.views import FoodFavourite, RestaurantFavourite
+from favourites.views import FoodFavourite, RestaurantFavourite, MyFavourites
 
 
 app_name = "favourites"
@@ -8,4 +8,5 @@ app_name = "favourites"
 urlpatterns = [
     path("foods/<int:pk>/", FoodFavourite.as_view(), name="food-toggle-favourite"),
     path("restaurants/<int:pk>/", RestaurantFavourite.as_view(), name="restaurant-toggle-favourite"),
+    path("my-favourites/", MyFavourites.as_view(), name="my_favourite")
 ]
